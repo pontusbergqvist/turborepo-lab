dd4ede36b9dfb3e8211d0708078bc2866e62fef0
Pontus Bergqvist <ponber@ponber-laptop.sector5.local>
2023-01-19 16:39:07 +0100

    added heart icon as a component

---
+++ apps/web/src/components/shared/icons/favorite.tsx
@@ -0,0 +1,11 @@
+import { AiFillHeart } from "react-icons/ai";
+
+const Favorite = () => {
+  return (
+    <div className="border-2 text-accent border-accent rounded-full">
+      <AiFillHeart />
+    </div>
+  );
+};
+
+export default Favorite;
