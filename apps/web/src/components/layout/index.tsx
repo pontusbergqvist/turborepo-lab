9d2fd14033916032679c43f31c29800bdc7926ff
Pontus Bergqvist <ponber@ponber-laptop.sector5.local>
2023-01-19 16:38:55 +0100

    changed width wrapper

--- apps/web/src/components/layout/index.tsx
+++ apps/web/src/components/layout/index.tsx
@@ -6,33 +6,35 @@ import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-primary flex flex-col min-h-screen h-full mx-auto">
-      <div className="p-3">
-        <Header />
-        <main className="bg-red-secondary max-w-[1000px]">{children}</main>
+    <div className="bg-primary">
+      <div className="flex flex-col max-w-[1200px] min-h-screen h-full pb-[70px] mx-auto">
+        <div className="p-3">
+          <Header />
+          <main className="mx-auto">{children}</main>
+        </div>
+        <nav className="w-full fixed bottom-0 left-0 bg-secondary p-2">
+          <ul className="flex justify-between max-w-[450px] mx-auto">
+            <Link href="/sok">
+              <li className="flex flex-col items-center">
+                <GiMagnifyingGlass className="text-[30px]" />
+                <p className="text-[14px] uppercase">Sök EVM</p>
+              </li>
+            </Link>
+            <Link href="/kategorier">
+              <li className="flex flex-col items-center">
+                <GoBook className="text-[30px]" />
+                <p className="text-[14px] uppercase">Kategorier</p>
+              </li>
+            </Link>
+            <Link href="/favoriter">
+              <li className="flex flex-col items-center">
+                <AiFillHeart className="text-[30px]" />
+                <p className="text-[14px] uppercase">Favoriter</p>
+              </li>
+            </Link>
+          </ul>
+        </nav>
       </div>
-      <nav className="w-full mt-auto bg-secondary p-2">
-        <ul className="flex justify-between max-w-[450px] mx-auto">
-          <Link href="/sok">
-            <li className="flex flex-col items-center">
-              <GiMagnifyingGlass className="text-[30px]" />
-              <p className="text-[14px] uppercase">Sök EVM</p>
-            </li>
-          </Link>
-          <Link href="/kategorier">
-            <li className="flex flex-col items-center">
-              <GoBook className="text-[30px]" />
-              <p className="text-[14px] uppercase">Kategorier</p>
-            </li>
-          </Link>
-          <Link href="/favoriter">
-            <li className="flex flex-col items-center">
-              <AiFillHeart className="text-[30px]" />
-              <p className="text-[14px] uppercase">Favoriter</p>
-            </li>
-          </Link>
-        </ul>
-      </nav>
     </div>
   );
 };
